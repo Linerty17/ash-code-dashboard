7ac10fb61d098ff63e0daf009166873f7d2e8416
Linerty17 <sundaychinemerem66@gmail.com>
2025-04-30 04:20:13 +0100

    Update TopUpPage.tsx

--- src/pages/TopUpPage.tsx
+++ src/pages/TopUpPage.tsx
@@ -46,7 +46,7 @@ const TopUpPage = () => {
       if (!isValid) {
         toast({
           title: "Invalid Access Code",
-          description: "The access code you entered is invalid. The correct code is 200718.",
+          description: "The access code you entered is invalid. use the correct code.",
           variant: "destructive",
         });
         setIsSubmitting(false);
